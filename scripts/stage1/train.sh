707a49101b550a4d93aafa301babec67b00e26b1
tuanmanh1410 <tuanmanh.bka@gmail.com>
2023-07-17 15:41:29 +0900

    Update scripts

--- scripts/stage1/train.sh
+++ scripts/stage1/train.sh
@@ -1,6 +1,6 @@
 python -m torch.distributed.launch \
         --master_port 47749 \
-        --nproc_per_node=8 main.py \
+        --nproc_per_node=4 main.py \
         --accumulate_steps 1 \
-        --lr_backbone 1e-5 --lr 5e-5 --num_queries 200 \
+        --lr_backbone 1e-5 --lr 1e-4 --num_queries 200 \
         --dataset_config configs/vidvrd_stage1.json
--- scripts/stage2/train.sh
+++ scripts/stage2/train.sh
@@ -1,6 +1,6 @@
 python -m torch.distributed.launch \
         --master_port 47745 \
-        --nproc_per_node=1 main.py \
+        --nproc_per_node=4 main.py \
         --accumulate_steps 1 \
         --lr_backbone 1e-5 --lr 5e-5 --num_queries 200 \
         --dataset_config configs/vidvrd_stage2.json
